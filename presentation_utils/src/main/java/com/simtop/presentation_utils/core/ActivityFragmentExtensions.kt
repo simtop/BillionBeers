c0e207d046444d60b5e3e517bb5b7f31f81dbd44
simtop <simtop@live.com>
2025-12-06 19:25:47 +0100

    Cleaning up more xml unused things.

--- presentation_utils/src/main/java/com/simtop/presentation_utils/core/ActivityFragmentExtensions.kt
+++ presentation_utils/src/main/java/com/simtop/presentation_utils/core/ActivityFragmentExtensions.kt
@@ -10,42 +10,6 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
-fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit) {
-  liveData.observe(this, Observer(body))
-}
-
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
   Toast.makeText(this, message, duration).show()
 }
-
-fun Context.dialog(
-  @StringRes titleRes: Int? = null,
-  @StringRes message: Int? = null,
-  builder: AlertDialog.Builder.() -> Unit
-): AlertDialog.Builder =
-  AlertDialog.Builder(this).apply {
-    titleRes?.let { setTitle(it) }
-    message?.let { setMessage(it) }
-    builder()
-  }
-
-fun AlertDialog.Builder.positiveButton(
-  @StringRes resTitle: Int,
-  func: AlertDialog.Builder.() -> Unit
-) {
-  setPositiveButton(resTitle) { _: DialogInterface?, _: Int -> func() }
-}
-
-fun AlertDialog.Builder.negativeButton(
-  @StringRes resTitle: Int,
-  func: AlertDialog.Builder.() -> Unit
-) {
-  setNegativeButton(resTitle) { _: DialogInterface?, _: Int -> func() }
-}
-
-fun AlertDialog.Builder.setLayout(viewRes: View) {
-  setView(viewRes)
-}
-
-fun getDialog(context: Context, title: Int? = null, message: Int? = null, layout: View) =
-  context.dialog(title, message) { setLayout(layout) }.setCancelable(false).create()
