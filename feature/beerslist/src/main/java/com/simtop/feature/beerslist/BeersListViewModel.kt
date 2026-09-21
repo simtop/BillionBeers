f000447fa9ec83281578ba714e58b5427235476b
simtop <simtop@live.com>
2026-09-21 09:01:45 +0200

    fix: repair shared ViewModel DI and CI diagnosis comments

--- feature/beerslist/src/main/java/com/simtop/feature/beerslist/BeersListViewModel.kt
+++ feature/beerslist/src/main/java/com/simtop/feature/beerslist/BeersListViewModel.kt
@@ -1,14 +1,16 @@
 package com.simtop.feature.beerslist
 
+import androidx.lifecycle.ViewModel
 import com.simtop.beerdomain.domain.repositories.BeersPagerFactory
 import com.simtop.beerdomain.domain.repositories.BeersRepository
 import com.simtop.billionbeers.shared.beerslist.BeersListViewModel as SharedBeersListViewModel
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.binding
 import dev.zacsweers.metrox.viewmodel.ViewModelKey
 
-@ContributesIntoMap(AppScope::class)
+@ContributesIntoMap(AppScope::class, binding = binding<ViewModel>())
 @ViewModelKey(BeersListViewModel::class)
 @Inject
 class BeersListViewModel(
--- feature/favorites/src/main/java/com/simtop/feature/favorites/FavoritesViewModel.kt
+++ feature/favorites/src/main/java/com/simtop/feature/favorites/FavoritesViewModel.kt
@@ -1,13 +1,15 @@
 package com.simtop.feature.favorites
 
+import androidx.lifecycle.ViewModel
 import com.simtop.beerdomain.domain.repositories.BeersRepository
 import com.simtop.billionbeers.shared.favorites.FavoritesViewModel as SharedFavoritesViewModel
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.binding
 import dev.zacsweers.metrox.viewmodel.ViewModelKey
 
-@ContributesIntoMap(AppScope::class)
+@ContributesIntoMap(AppScope::class, binding = binding<ViewModel>())
 @ViewModelKey(FavoritesViewModel::class)
 @Inject
 class FavoritesViewModel(beersRepository: BeersRepository) :
